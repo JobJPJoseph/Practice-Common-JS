27ef23a46dde39d797d404a8028f0c66dcdd40a7
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2023-11-14 23:55:54 -0500

    We finished the assigment.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 // Your code here
+const sayHelloTo = require('./send-messages/say-hello-to');
+const { message1: msg1, message2: msg2, message3: msg3 } = require('./messages');
+const test = require('./send-messages/give-message-to-mrs-potato');
 
+function giveMessageToMrsPotato(message) {
+    test(message);
+    return null;
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
